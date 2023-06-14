19fa21ba0115d3734ade1d471d41bd6f44acd254
girithc <girithmchoudhary@gmail.com>
2023-06-14 14:26:21 +0530

    update cors

--- src/main.ts
+++ src/main.ts
@@ -11,8 +11,8 @@ async function bootstrap() {
   );
   app.enableCors({
     origin: [
-      'https://bharat-apply.herokuapp.com',
-      '/.bharat-apply.herokuapp.com$/',
+      'https://bharatapply.herokuapp.com',
+      '/.bharatapply.herokuapp.com$/',
       'https://bharatapply-backend.herokuapp.com/token',
       '/.bharat-apply-backend.herokuapp.com/token$/',
       'http://localhost:4200',
